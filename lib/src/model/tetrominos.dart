743a65165e52f2fa20ce115c5d01bf7ac0ed5b4a
Aditya Dwivedi <dwikaljit@gmail.com>
2024-04-06 09:14:25 +0530

    wrote Z and S blocks.

--- lib/src/model/tetrominos.dart
+++ lib/src/model/tetrominos.dart
@@ -74,42 +74,73 @@ class LTetromino extends Tetromino {
     tiles[0] = Tile(axisX - 1, axisY);
     tiles[1] = Tile(axisX, axisY);
     tiles[2] = Tile(axisX + 1, axisY);
-    tiles[3] = Tile(axisX + 1, axisY + 1);
+    tiles[3] = Tile(axisX + 1, axisY - 1);
 
     axisOfRotationTile = tiles[1];
 
-    color = 'blue';
+    color = 'orange';
   }
 }
 
+/*
+T Block
+    =
+= = =
+*/
+class TTetromino extends Tetromino {
+  TTetromino(super.tiles, super.axisOfRotationTile, super.color, int width) {
+    final int axisX = width ~/ 2;
+    final int axisY = 0;
 
+    tiles[0] = Tile(axisX - 1, axisY);
+    tiles[1] = Tile(axisX, axisY);
+    tiles[2] = Tile(axisX + 1, axisY);
+    tiles[3] = Tile(axisX, axisY - 1);
 
+    axisOfRotationTile = tiles[1];
 
+    color = 'purple';
+  }
+}
 
+/*
+Z Block
+= =
+  = =
+*/
+class ZTetromino extends Tetromino {
+  ZTetromino(super.tiles, super.axisOfRotationTile, super.color, int width) {
+    final int axisX = width ~/ 2;
+    final int axisY = 0;
 
+    tiles[0] = Tile(axisX - 1, axisY - 1);
+    tiles[1] = Tile(axisX, axisY - 1);
+    tiles[2] = Tile(axisX, axisY);
+    tiles[3] = Tile(axisX + 1, axisY);
 
+    axisOfRotationTile = tiles[1];
 
+    color = 'red';
+  }
+}
 
+/*
+S Block
+  = =
+= =
+*/
+class STetromino extends Tetromino {
+  STetromino(super.tiles, super.axisOfRotationTile, super.color, int width) {
+    final int axisX = width ~/ 2;
+    final int axisY = 0;
 
+    tiles[0] = Tile(axisX - 1, axisY);
+    tiles[1] = Tile(axisX, axisY);
+    tiles[2] = Tile(axisX, axisY - 1);
+    tiles[3] = Tile(axisX + 1, axisY - 1);
 
+    axisOfRotationTile = tiles[1];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-_
+    color = 'green';
+  }
+}
